3bb012333e0d4cb4c93c5c4ddcdfec3695d6d75a
ffviana <francisco.viana@research.fchampalimaud.org>
2025-08-28 14:57:58 +0100

    finished cleaning up dev branch

--- __init__.py
+++ __init__.py
@@ -5,4 +5,4 @@ Top-level package for the Streamlit EMG app.
 Provides a single source of truth for the package version.
 """
 
-__all__ = ["steps", "utils", "tmp_module"]
+__all__ = ["steps", "utils",]
